e2a0b2f60e5980de2b3eb1dcfbf48f713b13be3c
BrunoMatosSilva <bmsdevfullstack@gmail.com>
2022-11-21 20:11:04 -0300

    first commit

---
+++ app/src/components/Categories/index.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
+import { Category} from '../../types/Category';
+import { Text } from '../Text';
+import { CategoryContainer, Icon } from './styles';
+
+interface CategoriesProps {
+  categories: Category[];
+  onSelectCategory: (categoryId: string) => Promise<void>;
+}
+
+export function Categories({ categories, onSelectCategory }: CategoriesProps){
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  function handleSelectedCategory(categoryId: string){
+    const category = selectedCategory === categoryId ? '' : categoryId;
+
+    onSelectCategory(category);
+    setSelectedCategory(category);
+  }
+
+  return (
+    <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{ paddingRight: 24 }}
+      data={categories}
+      keyExtractor={categories => categories._id}
+      renderItem={({item: category}) => {
+        const isSelected = selectedCategory === category._id;
+
+        return (
+          <CategoryContainer onPress={()=> handleSelectedCategory(category._id)}>
+            <Icon>
+              <Text opacity={isSelected ? 1 : 0.5 }>{category.icon}</Text>
+            </Icon>
+
+            <Text size={14} weight="600" opacity={isSelected ? 1 : 0.5 }>{category.name}</Text>
+          </CategoryContainer>
+        );
+      }}
+    />
+
+  );
+}
